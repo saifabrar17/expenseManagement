728c5b2f21321b30d15f2283f59e0c37db9e56d3
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-02-17 19:45:18 +0600

    almost done, needs improvement

--- js/script.js
+++ js/script.js
@@ -1,27 +1,65 @@
 
 // declaring a function for getting value as number
-function getParsedValue(idName){
-    var parsedValue = parseInt(document.getElementById(idName).value);
+function getParsedValueById(idName) {
+    let parsedValue = parseInt(document.getElementById(idName).value);
     return parsedValue;
 }
 
 // declaring function to get elements by id
-function getElement(idName){
-    var element = document.getElementById(idName);
+function getElement(idName) {
+    let element = document.getElementById(idName);
     return element;
 }
 
-function calculatingExpense(){
-    var incomeAmount = getParsedValue("input-income");
-    var foodCost = getParsedValue("input-food");
-    var rentCost = getParsedValue("input-rent");
-    var clothCost = getParsedValue("input-cloths");
-    var totalExpense = getElement("expense-total");
-    var balanceAfterExpense = getElement("balance-total");
+function calculatingExpense() {
+    let incomeAmount = getParsedValueById("input-income");
+    let foodCost = getParsedValueById("input-food");
+    let rentCost = getParsedValueById("input-rent");
+    let clothCost = getParsedValueById("input-cloths");
+    let totalExpense = getElement("expense-total");
+    let balanceAfterExpense = getElement("balance-remaining");
 
     // total cost
-    var totalCost = foodCost + rentCost + clothCost;
+    let totalCost = foodCost + rentCost + clothCost;
 
-    totalExpense.innerText = totalCost;
-    balanceAfterExpense.innertext = incomeAmount - totalCost;
+    //validity check
+    if (isNaN(incomeAmount) || incomeAmount < 0) {
+        alert("please provide a positiv income value");
+    } else if (isNaN(foodCost) || foodCost < 0) {
+        alert("please provide a positiv food value");
+    } else if (isNaN(rentCost) || rentCost < 0) {
+        alert("please provide a positiv rent value");
+    } else if (isNaN(clothCost) || clothCost < 0) {
+        alert("please provide a positiv cloth value");
+    } else if (incomeAmount < totalCost) {
+        alert("surpassed your income");
+    }
+    else {
+        totalExpense.innerText = totalCost;
+        balanceAfterExpense.innerText = incomeAmount - totalCost;
+    }
+}
+
+
+function calculateSavings() {
+
+    let balanceAfterExpense = getElement("balance-remaining");
+    let totalExpense = getElement("expense-total");
+    let incomeAmount = getParsedValueById("input-income");
+    let savingPercentage = getParsedValueById("input-saving-percentage");
+    let savingAmount = getElement("saving-amount");
+    let remainBalance = getElement("final-remaining");
+
+    let savings = (savingPercentage / 100) * incomeAmount;
+    let finalBalance = incomeAmount - (savings + parseInt(totalExpense.innerText));
+
+    if (isNaN(savingPercentage) || savingPercentage < 0) {
+        alert("percentage cant be negative");
+    } else if (savings > parseInt(balanceAfterExpense.innerText)) {
+        alert("you cant save mote than you hbve");
+    } else {
+        savingAmount.innerText = savings;
+        remainBalance.innerText = finalBalance;
+    }
+
 }
